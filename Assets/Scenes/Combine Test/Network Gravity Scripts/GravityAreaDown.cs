98818c480df148aecc1a4f673e9115c956bd88f7
flyt9097 <78530498+flyt9097@users.noreply.github.com>
2024-04-12 17:30:15 +0900

    Register List Check

---
+++ Assets/Scenes/Combine Test/Network Gravity Scripts/GravityAreaDown.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GravityAreaDown : GravityArea
+{
+    public override Vector3 GetGravityDirection(GravityBody _gravityBody)
+    {
+        return transform.up;
+    }
+}
