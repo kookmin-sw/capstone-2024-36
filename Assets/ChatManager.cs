e27dbf31d6086f22b86db02e75e709b6539eb85c
kipum Kang <kipum50@naver.com>
2024-05-03 02:38:17 +0900

    스크롤 뷰 위치 고정
    수정 전

--- Assets/ChatManager.cs
+++ Assets/ChatManager.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 using TMPro; // TextMeshPro를 사용하기 위한 네임스페이스
+using UnityEngine.UI; // ScrollRect를 사용하기 위한 네임스페이스
+using System.Collections; // 코루틴 사용을 위한 네임스페이스
 
 public class ChatManager : MonoBehaviour
 {
     public static ChatManager Instance;  // 싱글톤 인스턴스
     public TextMeshProUGUI chatText;     // 채팅창에 텍스트를 표시할 TextMeshProUGUI
+    public ScrollRect scrollRect;        // 스크롤뷰의 ScrollRect 컴포넌트
 
     void Awake()
     {
+        scrollRect = GetComponent<ScrollRect>();
         // 싱글톤 패턴 구현
         if (Instance == null)
         {
@@ -25,5 +29,14 @@ public class ChatManager : MonoBehaviour
     {
         // 채팅창의 텍스트에 새 메시지를 추가
         chatText.text += message + "\n";
+        // 스크롤 위치 조정을 위한 코루틴 호출
+        StartCoroutine(AdjustScrollPosition());
+    }
+
+    // 스크롤 위치를 맨 아래로 조정하는 코루틴
+    private IEnumerator AdjustScrollPosition()
+    {
+        yield return new WaitForSeconds(0.1f);  // 짧은 딜레이 후 실행
+        scrollRect.verticalNormalizedPosition = 0f;  // 스크롤을 맨 아래로 조정
     }
 }
