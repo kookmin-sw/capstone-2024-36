2782409dcf53adf55e4b4beb73f82a439e5b00c8
vxbdfsg <39340989+vxbdfsg@users.noreply.github.com>
2024-05-14 04:09:29 +0900

    크기맵 퍼즐 완성 / 자잘한 버그 및 광원 추가
    크기맵 퍼즐 완성 / 자잘한 버그 및 광원 추가

--- Assets/Scenes/Combine Test/Scripts/ReciprocatingMotion.cs
+++ Assets/Scenes/Combine Test/Scripts/ReciprocatingMotion.cs
@@ -6,7 +6,8 @@ public class ReciprocatingMotion : MonoBehaviour
 {
     Vector3 pos; //현재위치
     [SerializeField] float delta = 8.0f; // 좌(우)로 이동가능한 (x)최대값
-    [SerializeField] float speed = 3.0f; // 이동속도
+    [SerializeField] float Xspeed = 3.0f; // 이동속도
+    [SerializeField] float Yspeed = 0.0f; // 이동속도
 
     void Start()
     {
@@ -17,7 +18,8 @@ public class ReciprocatingMotion : MonoBehaviour
     void Update()
     {
         Vector3 v = pos;
-        v.x += delta * Mathf.Sin(Time.time * speed);
+        v.x += delta * Mathf.Sin(Time.time * Xspeed);
+        v.y += delta * Mathf.Sin(Time.time * Yspeed);
         transform.position = v;
     }
 }
