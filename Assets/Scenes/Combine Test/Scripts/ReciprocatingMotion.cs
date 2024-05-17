3ce80fb9b8044982b71c970dc05dda61711b1722
vxbdfsg <39340989+vxbdfsg@users.noreply.github.com>
2024-05-17 17:19:01 +0900

    Revert "힌트 지연시간 설정"
    This reverts commit 12cc9a86661e39fde12ecc63bde7f7af1594963b.

--- Assets/Scenes/Combine Test/Scripts/ReciprocatingMotion.cs
+++ Assets/Scenes/Combine Test/Scripts/ReciprocatingMotion.cs
@@ -6,8 +6,7 @@ public class ReciprocatingMotion : MonoBehaviour
 {
     Vector3 pos; //현재위치
     [SerializeField] float delta = 8.0f; // 좌(우)로 이동가능한 (x)최대값
-    [SerializeField] float Xspeed = 3.0f; // 이동속도
-    [SerializeField] float Yspeed = 0.0f; // 이동속도
+    [SerializeField] float speed = 3.0f; // 이동속도
 
     void Start()
     {
@@ -18,8 +17,7 @@ public class ReciprocatingMotion : MonoBehaviour
     void Update()
     {
         Vector3 v = pos;
-        v.x += delta * Mathf.Sin(Time.time * Xspeed);
-        v.y += delta * Mathf.Sin(Time.time * Yspeed);
+        v.x += delta * Mathf.Sin(Time.time * speed);
         transform.position = v;
     }
 }
