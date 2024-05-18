5ef9d6f68aa05e700ca51cce34218f99cabbc48b
Seho-P <qkrtpgh77@gmail.com>
2024-05-18 20:35:32 +0900

    마지막 스테이지 검은색 레이저로/ 간단한 맵 수정 / 리스트 및 프리팹 추가/ 재작업

--- Assets/Scenes/Combine Test/Scripts/WallControl.cs
+++ Assets/Scenes/Combine Test/Scripts/WallControl.cs
@@ -7,12 +7,28 @@ public class WallControl : MonoBehaviour
 {
     public GameObject wall1;
     public bool isWall;
+    private Vector3 currentPosition;
+    public float speed = 5.0f;
 
     // Update is called once per frame
+    void Start(){
+        currentPosition = wall1.transform.position;
+    }
     void Update()
     {
         if(isWall){
-            wall1.SetActive(false);
+            if (currentPosition.x > -45)
+        {
+            wall1.transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+
+        // x좌표가 -45에 도달하거나 지나치지 않도록 합니다
+        if (wall1.transform.position.x < -45)
+        {
+            Vector3 newPosition = wall1.transform.position;
+            newPosition.x = -45;
+            wall1.transform.position = newPosition;
+        }
         }
     }
 
