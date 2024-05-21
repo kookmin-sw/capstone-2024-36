3a67991d0825bc0fb5e5afd48bee7941cfe5d5b2
kipum Kang <kipum50@naver.com>
2024-05-21 18:27:03 +0900

    대화가 한 번 발생한 후 다른 씬에 갔다 오면 다시 발생하지 않도록 설정

--- Assets/TutorialTrigger.cs
+++ Assets/TutorialTrigger.cs
@@ -4,33 +4,46 @@ using UnityEngine;
 public class TutorialTrigger : MonoBehaviour
 {
     public string message; // 표시할 메시지
-    public float delayTime = 0f; // 메시지 지연 시간 (초), 인스펙터에서 설정 가능
+    public float messageDelay = 0f; // 메시지 지연 시간
+    public string triggerID; // 트리거를 구별할 ID
     private bool hasTriggered = false;
+
+    private void Start()
+    {
+        // 트리거 ID가 없으면 게임 오브젝트 이름으로 설정
+        if (string.IsNullOrEmpty(triggerID))
+        {
+            triggerID = gameObject.name;
+        }
+
+        // 트리거가 이미 발생했는지 확인
+        hasTriggered = PlayerPrefs.GetInt(triggerID, 0) == 1;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        // 플레이어가 트리거에 들어올 때
         if ((other.CompareTag("PlayerForGravity") || other.CompareTag("moveable")) && !hasTriggered)
         {
-            // DontDestroyOnLoad 상태의 ChatManager 인스턴스를 찾음
-            ChatManager chatManager = DontDestroyOnLoadManager.FindChatManagerInDontDestroyOnLoad();
-            if (chatManager != null)
-            {
-                StartCoroutine(DelayedMessage(chatManager));
-                hasTriggered = true;
-            }
-            else
-            {
-                Debug.LogError("ChatManager not found in DontDestroyOnLoad scene.");
-            }
+            StartCoroutine(DisplayMessage());
         }
     }
 
-    private IEnumerator DelayedMessage(ChatManager chatManager)
+    private IEnumerator DisplayMessage()
     {
-        // 설정된 시간만큼 대기
-        yield return new WaitForSeconds(delayTime);
-        // 메시지를 추가
-        chatManager.AddMessage(message);
+        yield return new WaitForSeconds(messageDelay);
+
+        // DontDestroyOnLoad 상태의 ChatManager 인스턴스 찾기
+        ChatManager chatManager = DontDestroyOnLoadManager.FindChatManagerInDontDestroyOnLoad();
+        if (chatManager != null)
+        {
+            chatManager.AddMessage(message);
+            hasTriggered = true;
+            PlayerPrefs.SetInt(triggerID, 1); // 트리거 발생 여부 저장
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            Debug.LogError("ChatManager not found in DontDestroyOnLoad scene.");
+        }
     }
 }
